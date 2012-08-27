40b2d6e128b254712ff8c980228ca3e086f9cfa6
PierrickPuimeanChieze <pierrick.puimean@reschaotica.com>
2012-08-27 09:14:41 +0200

    Added an interface to Satellite

--- main/java/srcModel/com/newdawn/model/system/Satellite.java
+++ main/java/srcModel/com/newdawn/model/system/Satellite.java
@@ -4,11 +4,21 @@
  */
 package com.newdawn.model.system;
 
+import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import com.newdawn.model.mineral.MinerallyExploitableBody;
+import com.newdawn.model.mineral.MinerallyExploitableBodyModel;
+
 /**
  * 
  * @author Pierrick Puimean-Chieze
  */
-public class Satellite extends OrbitalBody {
+public class Satellite extends OrbitalBody implements MinerallyExploitableBody {
+
+	private StringProperty visualNameProperty;
+	private MinerallyExploitableBodyModel minerallyExploitableBodyModel;
 
 	public Satellite(String name, Orbit orbit, double delta,
 			Long orbitalPeriod, long diameter) {
@@ -24,5 +34,27 @@ public class Satellite extends OrbitalBody {
 	public Satellite(String name, Orbit orbit, long diameter) {
 		super(orbit, diameter);
 		setName(name);
+	}
+
+	@Override
+	public ReadOnlyStringProperty visualNameProperty() {
+		if (visualNameProperty == null) {
+			visualNameProperty = new SimpleStringProperty(this, "visualName");
+			visualNameProperty.bind(nameProperty());
+		}
+
+		return visualNameProperty;
+	}
+
+	@Override
+	public MinerallyExploitableBodyModel getMinerallyExploitableBodyModel() {
+		return minerallyExploitableBodyModel;
+	}
+
+	@Override
+	public void setMinerallyExploitableBodyModel(
+			MinerallyExploitableBodyModel minerallyExploitableBodyModel) {
+		this.minerallyExploitableBodyModel = minerallyExploitableBodyModel;
+
 	}
 }
