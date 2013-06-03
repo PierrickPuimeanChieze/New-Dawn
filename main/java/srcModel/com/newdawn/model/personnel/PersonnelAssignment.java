86745582be996c8a61776f5238975267d04b860c
Pierrick Puimean-Chieze <pierrick.puimean@reschaotica.com>
2013-06-03 16:42:14 +0200

    Updated the PersonnelAssignment : it has now a PersonnelLocalization, instead of being one

--- main/java/srcModel/com/newdawn/model/personnel/PersonnelAssignment.java
+++ main/java/srcModel/com/newdawn/model/personnel/PersonnelAssignment.java
@@ -1,8 +1,13 @@
 package com.newdawn.model.personnel;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyStringProperty;
+
 /**
  * 
  * @author Pierrick Puimean-Chieze
  */
-public interface PersonnelAssignment extends PersonnelLocalisation {
+public interface PersonnelAssignment {
+	public ReadOnlyStringProperty visualNameProperty();
+	public ObjectProperty<PersonnelLocalisation> localizationProperty();
 }
